f94833c3521667df9141b7a72372d0f139c44de4
Lygys <duc.de.alphaasile@gmail.com>
2022-02-10 11:18:35 +0000

    Userのviewを調整

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,6 @@
 class Public::UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update, :potential_followers]
+
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
@@ -47,6 +48,6 @@ class Public::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :profile_image, :introduction)
   end
 end
