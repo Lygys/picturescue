25abba79487e14e748579bd2a218afe82b61b184
Lygys <duc.de.alphaasile@gmail.com>
2022-02-11 09:38:21 +0000

    作業中断

--- app/controllers/public/tweets_controller.rb
+++ app/controllers/public/tweets_controller.rb
@@ -5,12 +5,15 @@ class Public::TweetsController < ApplicationController
     @tweet.user_id = current_user.id
     if @tweet.save
       redirect_to mypage_path(current_user), notice: "投稿が完了しました"
+    else
+
+    end
   end
 
   def index
     @tweet = Tweet.new
     @followings = current_user.followings
-    tweets = return @followings.inject(init = [current_user.tweets]) {|result, following| result + following.tweets }
+    tweets = @followings.inject(init = current_user.tweets){|result, following| result + following.tweets}
     @tweets = tweets.order(created_at: :desc).page(params[:page]).per(50)
   end
 
